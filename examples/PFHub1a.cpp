630e0face01d8f1917747156c55b6d02b19290cd
David <DavidJoy022@gmail.com>
2024-05-01 18:30:45 +0000

    Added new parsing to PFHub1a example

--- examples/PFHub1a.cpp
+++ examples/PFHub1a.cpp
@@ -1,3 +1,4 @@
+#include <CommandLine.hpp>
 #include <PFHub.hpp>
 
 using namespace CabanaPF;
@@ -7,26 +8,38 @@ int main(int argc, char* argv[]) {
     {
         Kokkos::ScopeGuard scope_guard(argc, argv);
         try {
-            if (argc != 5)
+            CommandLineInput parser;
+            const int arg_start = parser.read_command_line(argc, argv);
+            if (argc == arg_start) // no problem name specified
                 throw std::invalid_argument("");
-            const int grid_points = std::stoi(argv[2]);
-            const double dt = std::stod(argv[3]);
-            const double end_time = std::stod(argv[4]);
 
             // read in the type of simulation to run:
             std::unique_ptr<PFHub1aBase> simulation;
-            std::string problem_name(argv[1]);
-            if (problem_name == "benchmark")
-                simulation = std::make_unique<PFHub1aBenchmark2017>(grid_points, dt);
+            std::string problem_name(argv[arg_start]);
+            if (problem_name == "2017")
+                simulation = std::make_unique<PFHub1aBenchmark2017>(parser.grid_points, parser.dt);
             else if (problem_name == "2023")
-                simulation = std::make_unique<PFHub1aCHiMaD2023>(grid_points, dt);
-            else
-                throw std::invalid_argument("");
+                simulation = std::make_unique<PFHub1aCHiMaD2023>(parser.grid_points, parser.dt);
+            else if (problem_name == "custom") {
+                const int N_COEFFICIENTS = 10;
+                if (argc < arg_start + N_COEFFICIENTS + 1)
+                    throw std::invalid_argument("too few coefficients (need 10) for custom");
+                // read in coefficients:
+                std::array<int, N_COEFFICIENTS> Ns;
+                for (int i = 0; i < N_COEFFICIENTS; i++)
+                    Ns[i] = std::stoi(argv[arg_start + i + 1]);
+                // make problem:
+                simulation = std::make_unique<PFHub1aCustom>(parser.grid_points, parser.dt, Ns[0], Ns[1], Ns[2], Ns[3],
+                                                             Ns[4], Ns[5], Ns[6], Ns[7], Ns[8], Ns[9]);
+            } else
+                throw std::invalid_argument("unrecognized problem name");
 
-            simulation->run_until_time(end_time);
-            simulation->output_c();
-        } catch (std::invalid_argument const&) {
-            std::cout << "Usage: ./PFHub1a <benchmark|2023> <grid points> <dt> <end time>" << std::endl;
+            parser.add_outputs_to_runner(*simulation);
+            simulation->run_until_time(parser.end_time);
+        } catch (const std::invalid_argument& e) {
+            std::cerr << e.what() << std::endl;
+            std::cerr << "Usage: ./PFHub1a" << CommandLineInput::USAGE << " <2017|2023|custom> [custom coefficients]"
+                      << std::endl;
         }
     }
     MPI_Finalize();
